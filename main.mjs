160f73c3f05819c97a4cac1a240188346d51ea86
George Witteman <george@witteman.me>
2023-05-03 18:02:28 -0400

    mjs and other stuff

--- main.mjs
+++ main.mjs
@@ -1,9 +1,12 @@
-import { html, render, useState, useEffect } from 'https://unpkg.com/htm@3.1.1/preact/standalone.module.js'
-import { test } from "./test.js";
-import test2 from "./test2.js";
+import { h, html, render, useState, useEffect } from 'https://unpkg.com/htm@3.1.1/preact/standalone.module.js'
+import test2 from "./test2.mjs";
+
+const {test} = await import("./test.mjs");
 
 console.log(test("hi"));
 console.log(test2("hi2"));
+
+console.log(await Promise.resolve("awaited"));
 
 // https://github.com/preactjs/preact-router
 // https://www.typescriptlang.org/docs/handbook/intro-to-js-ts.html
@@ -14,21 +17,23 @@ console.log(test2("hi2"));
  *
  * @param {object} props
  * @param {string} props.name
- * @param {string} props.foo
+ * @param {string} props.title
  * @returns {VNode}
  */
 function App(props) {
   const [oldTitle] = useState(document.title);
   useEffect(() => {
-    document.title = props.foo;
+    document.title = props.title;
 
     return () => {
       document.title = oldTitle;
     }
-  }, [props.foo])
-  return html`<h1>Hello ${props.name} ${props.foo}!</h1>`;
+  }, [props.title])
+  return html`<h1>Hello ${props.name} ${props.title}!</h1>`;
 }
 
 const root = document.getElementById("app");
 root.innerHTML = ""
-render(html`<${App} name="World" foo="bar" />`, root);
+// html`<${App} name="World" title="bar" />`
+const rootElement = h(App, { name: "World", title: "bar" });
+render(rootElement, root);
