a2ccafb5b6ee870acfcf271b1d3b276b7af1656f
George Witteman <george@witteman.me>
2023-05-05 22:53:20 +0000

    Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <p className="max-w-lg"><Link href="/test">Test</Link><Image
-                 src="/vercel.svg"
-                 alt="Vercel Logo"
-                 className="dark:invert"
-                 width={100}
-                 height={24}
-                 priority
-               /></p>
-  )
+    <p className="max-w-lg">
+      <Link href="/test">Test</Link>
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        className="dark:invert"
+        width={100}
+        height={24}
+        priority
+      />
+    </p>
+  );
 }
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function Test() {
-  return (<div>Test test test</div>)
+  return (
+    <div>
+      Test test test <Link href="/">Back</Link>
+    </div>
+  );
 }
